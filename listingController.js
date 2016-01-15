08ef96309edb721190040760efab6ed0f6a73c46
aumulloa <aumulloa@gmail.com>
2016-01-14 23:15:38 -0500

    Made many changes, table works, details too (sortof)

--- listingController.js
+++ listingController.js
@@ -1,14 +1,23 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
+      $scope.addListing = function() {
+          $scope.lisings.push($scope.newItem);
+          $scope.newItem = '';
+
+      };
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.deleteListing = function(index) {
+        $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+        $scope.newItem = $scope.listings[index];
+    };
   }
 ]);
