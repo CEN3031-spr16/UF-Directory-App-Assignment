bed3a7e34e000c31c84f3eb42cb6881f24c67b6a
aumulloa <aumulloa@gmail.com>
2016-01-15 11:43:58 -0500

    Search bar works, missing Add directive form

--- listingController.js
+++ listingController.js
@@ -6,13 +6,13 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     /*
       Implement these functions in the controller to make your application function
       as described in the assignment spec.
-      $scope.addListing = function() {
-          $scope.lisings.push($scope.newItem);
-          $scope.newItem = '';
 
-      };
      */
+     $scope.addListing = function() {
+         $scope.lisings.push($scope.newItem);
+         $scope.newItem = '';
 
+     };
     $scope.deleteListing = function(index) {
         $scope.listings.splice(index, 1);
     };
