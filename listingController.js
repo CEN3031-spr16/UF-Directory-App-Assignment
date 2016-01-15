2367d66f107ee0d6c9a166e5af125e98fb046e52
jeffrey mclemore <jmclemore368@gmail.com>
2016-01-14 20:47:55 -0500

    finished first 2 sections

--- listingController.js
+++ listingController.js
@@ -1,14 +1,53 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    //For the submitting of a new item
+    $scope.newItem = {
+      code: '', //Take chars
+      name: '',
+      coordinates: { //Wrap in coordinates object like in listingFactory.js
+        latitude: 0,
+        longitude: 0 //Lat/Long take numbers not chars
+      },
+      address: ''
+    };
+
+    $scope.addItem = function() {
+      //Create new object with data from form
+      var newListingToInsert = {
+        code: $scope.newItem.code,
+        name: $scope.newItem.name,
+        coordinates: {
+          latitude: $scope.newItem.coordinates.latitude,
+          longitude: $scope.newItem.coordinates.longitude
+        },
+        address: $scope.newItem.address,
+      }
+      //Add to array of listings
+      $scope.listings.push(newListingToInsert);
+      //Reset the form
+      reset();
+    };
+
+    //Resets the form when called
+    var reset = function() {
+      $scope.newItem.code = '';
+      $scope.newItem.name = '';
+      $scope.newItem.coordinates.latitude = 0;
+      $scope.newItem.coordinates.longitude = 0;
+      $scope.newItem.address = '';
+    };
+    //Need to reset on load else shows empty objects
+    reset();
+
+    $scope.deleteItem = function(index) {
+      //The splice() method adds/removes items to/from an array
+      //http://www.w3schools.com/jsref/jsref_splice.asp
+      $scope.listings.splice(index, 1);
+    };
+
+
   }
 ]);
